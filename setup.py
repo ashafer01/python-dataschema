b333df8efa75472c1fb5785172b01e0c8de16aad
Alex Shafer <ashafer01@gmail.com>
2019-12-05 08:11:23 -0700

    fix setup.py url

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Alex Shafer',
     author_email='ashafer@pm.me',
-    url='https://github.com/ashafer01/dataschema',
+    url='https://github.com/ashafer01/python-dataschema',
     license='MIT',
     description='DataSchema schema checking for arbitrary data',
     long_description='Define schemas for your data or configurations, and validate and normalize them in one step.',
